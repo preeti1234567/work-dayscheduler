ec40c11ef8576f1e9dc18774013d3ec380c559d8
Preeti Gupta <preetigupta510@gmail.com>
2020-06-25 08:37:06 -0700

    added current date in the header and added some functionality in the save button

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 $(document).ready(function() {
-            $("#currentDay").text(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
+    $("#currentDay").text(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
 
+    $(".fa-edit").on("click", save);
 
-
-
-        }
+    function save() {
+        alert($(this).attr('value'));
+    }
+});
